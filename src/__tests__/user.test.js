7f13f4ebfc028a242bff0bd33c3f513ccc194fee
Sayfudin90 <Seifdin62@gmail.com>
2025-01-15 21:39:58 +0300

    Pass user test by exporting username, city, and image

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,6 +1,7 @@
+// src/__tests__/user.test.js
 import { username, city, image } from "../data/user";
 
-test("username, city, and image are all exported", () => {
+test("Liza, city, and image are all exported", () => {
   expect(username).toEqual("Liza");
   expect(city).toEqual("New York");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
